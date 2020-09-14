b3c2b39ff7d03c54688ebff684b3f642af21ade9
Pedro Sampaio <pedroigorjs@gmail.com>
2020-09-14 10:45:01 -0300

    Create Messages component

---
+++ react/Messages.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+
+
+
+interface MessagesProps {
+  timezone: string;
+  locale: string;
+}
+
+const Messages: StorefrontFunctionComponent<MessagesProps> = ({ timezone, locale }) => {
+  return (
+    <div className="ph2 overflow-hidden overflow-y-scroll mb5 pb2">
+    </div>
+  )
+}
+
+Messages.schema = {
+  title: "Chatbot Messages",
+  description: "A component that wrap all messages.",
+  type: "object",
+  properties: {
+    timezone: {
+      title: "Message Timezone",
+      description: "A timezone for message time, example: America/Sao_Paulo, America/Los_Angeles",
+      type: "number",
+      default: null
+    },
+    locale: {
+      title: "Message Locale",
+      description: "A locale for message time, example: pt-BR, en-US, etc...",
+      type: "string",
+      default: null
+    }
+  }
+}
+
+export default Messages
